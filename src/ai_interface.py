6877efd1348b426cb921706e056217c968d8618a
Alireza Farshin <farshin@kth.se>
2023-05-30 20:14:55 +0200

    AIInterface refactored (introducing an abstract class)

--- src/ai_interface.py
+++ src/ai_interface.py
@@ -1,3 +1,4 @@
+import abc
 import bardapi
 import openai
 from EdgeGPT import Query, Cookie
@@ -6,23 +7,88 @@ import os
 import sys
 
 
-class AIInterface:
-    def __init__(self, ai_choice):
-        self.ai_choice = ai_choice
-        if ai_choice == 'BARD':
-            self.bard_init()
-        elif ai_choice == 'BING':
-            self.bing_init()
-        elif ai_choice == 'GPT':
-            self.gpt_init()
+class AIInterface(abc.ABC):
+    def __init__(self):
+        self.init()
 
-    def bard_init(self):
-        # os.environ['_BARD_API_KEY'] = "placeholder"\
-        "
+    @abc.abstractmethod
+    def init(self):
+        pass
+
+    @abc.abstractmethod
+    def submit_task(self, task, code, role=""):
+        pass
+
+    @abc.abstractmethod
+    def get_code(self, response):
+        pass
+
+
+class Bard(AIInterface):
+    def init(self):
+        # os.environ['_BARD_API_KEY'] = "placeholder"
         bardapi.api_key = os.environ.get('_BARD_API_KEY', 'Not Set')
         self.bard = bardapi.core.Bard()
 
-    def bing_init(self):
+    def submit_task(self, task, code, role=""):
+        # Create a prompt
+        prompt = task + "\n" + code
+        # Get response from BARD
+        response = self.bard.get_answer(prompt)
+        # Extract the code from the response and return it
+        if ('Error' in response.get('content')):
+            print(Fore.RED + "Error in BARD API." + Style.RESET_ALL)
+            sys.exit(1)
+        return self.get_code(response.get('content'))
+
+    def get_code(self, response):
+        index = response.find("```")
+        code = response[index:].strip()
+        index = code.find("c++")
+        code = code[index+3:].strip()
+        index = code.find("```")
+        code = code[:index].strip()
+        return code
+
+
+class GPT(AIInterface):
+    def init(self):
+        # os.environ['OPENAI_API_KEY'] = "placeholder"
+        openai.api_key = os.environ.get('OPENAI_API_KEY', 'Not Set')
+        self.gpt = openai
+        self.model = "gpt-3.5-turbo"
+        '''A temperature of 0 means the responses will be very straightforward, almost deterministic (meaning you almost always get the same response to a given prompt)
+        A temperature of 1 means the responses can vary wildly.'''
+        self.temperature = 0
+        self.submit_task("Always give me only code with syntax highlighting; for example ```c++ int main()```", "", "system")
+
+    def submit_task(self, task, code, role="user"):
+        # Create a prompt
+        prompt = task + "\n" + code
+        messages = [{"role": role, "content": prompt}]
+        # Get response from GPT
+        response = self.gpt.ChatCompletion.create(
+            model=self.model,
+            messages=messages,
+            temperature=self.temperature,)
+        # Extract the code from the response and return it
+        if ('Error' in response):
+            print(Fore.RED + "Error in OpenAI API." + Style.RESET_ALL)
+            sys.exit(1)
+        return self.get_code(response.choices[0].message["content"])
+
+    def get_code(self, response):
+        index = response.find("```")
+        code = response[index:].strip()
+        index = code.find("c++")
+        code = code[index+3:].strip()
+        index = code.find("```")
+        code = code[:index].strip()
+        return code
+
+
+class Bing(AIInterface):
+    def init(self):
         os.environ['BING_U'] = ""
         self.bing = Query
         self.style = "precise"  # creative, balanced, or precise
@@ -34,80 +100,26 @@ class AIInterface:
         # echo - Print something to confirm request made
         # echo_prompt - Print confirmation of the evaluated prompt
 
-    def gpt_init(self):
-        # os.environ['OPENAI_API_KEY'] = "placeholder"\
-        "
-        openai.api_key = os.environ.get('OPENAI_API_KEY', 'Not Set')
-        self.gpt = openai
-        self.model = "gpt-3.5-turbo"
-        '''A temperature of 0 means the responses will be very straightforward
-        A temperature of 1 means the responses can vary wildly.'''
-        self.temperature = 0
-        self.system_prompt = "Always give me only code with syntax"\
-            " highlighting; for example ```c++ int main()```"
-        self.submit_task_gpt(self.system_prompt, "", "system")
-
-    def submit_task(self, task, code):
-        if self.ai_choice == 'GPT':
-            return self.submit_task_gpt(task, code)
-        elif self.ai_choice == 'BARD':
-            return self.submit_task_bard(task, code)
-        elif self.ai_choice == 'BING':
-            return self.submit_task_bing(task, code)
-
-    # TODO: Check this function with the GPT API
-    def get_code_gpt(self, input):
-        index = input.find("```")
-        code = input[index:].strip()
-        index = code.find("c++")
-        code = code[index+3:].strip()
-        index = code.find("```")
-        code = code[:index].strip()
-        return code
-
-    def submit_task_gpt(self, task, code, role="user"):
-        # Create a prompt
-        prompt = task + "\n" + code
-        messages = [{"role": role, "content": prompt}]
-        # Get response from GPT
-        response = self.gpt.ChatCompletion.create(
-            model=self.model,
-            messages=messages,
-            temperature=self.temperature)
-        # Extract the code from the response and return it
-        if ('Error' in response):
-            print(Fore.RED + "Error in OpenAI API." + Style.RESET_ALL)
-            sys.exit(1)
-        return self.get_code_gpt(response.choices[0].message["content"])
-
-    def get_code_bard(self, input):
-        index = input.find("```")
-        code = input[index:].strip()
-        index = code.find("c++")
-        code = code[index+3:].strip()
-        index = code.find("```")
-        code = code[:index].strip()
-        return code
-
-    def submit_task_bard(self, task, code):
-        # Create a prompt
-        prompt = task + "\n" + code
-        # Get response from BARD
-        response = self.bard.get_answer(prompt)
-        # Extract the code from the response and return it
-        if ('Error' in response.get('content')):
-            print(Fore.RED + "Error in BARD API." + Style.RESET_ALL)
-            sys.exit(1)
-        return self.get_code_bard(response.get('content'))
-
-    def submit_task_bing(self, task, code):
+    def submit_task(self, task, code, role=""):
         # Create a prompt
         prompt = task + "\n" + code
         # Get response from Bing
-        response = self.bing(prompt, self.style, self.content_type, 0,
-                             echo=True, echo_prompt=True)
+        response = self.bing(prompt, self.style, self.content_type,
+                             0, echo=False, echo_prompt=False)
         # Extract the code from the response and return it
         if ('Error' in response.output):
             print(Fore.RED + "Error in Bing API." + Style.RESET_ALL)
             sys.exit(1)
+        return self.get_code(response)
+
+    def get_code(self, response):
         return response.code
+
+
+def AIBuilder(ai_choice):
+    if ai_choice == 'BARD':
+        return Bard()
+    elif ai_choice == 'BING':
+        return Bing()
+    elif ai_choice == 'GPT':
+        return GPT()

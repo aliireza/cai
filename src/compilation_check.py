a0c271c781cac47d516e24eae11eecb5a3c61a44
Alireza Farshin <farshin@kth.se>
2023-05-30 20:45:17 +0200

    added comments

--- src/compilation_check.py
+++ src/compilation_check.py
@@ -2,29 +2,34 @@ import subprocess
 from colorama import Fore, Style
 
 
+# This class is used to compile code, check for errors and fix them using an AI interface
 class CompilationCheck:
     def __init__(self, ai, lagnuage, compiler):
-        self.language = lagnuage
-        self.compiler = compiler
+        self.language = lagnuage  # Programming language
+        self.compiler = compiler  # Compiler to use
         self.prompt = "If there is no main, add it and use this" \
             + "code/function. If there are compilation errors, fix all of them: "
-        self.ai = ai
-        self.count = 0
+        self.ai = ai  # AI interface to use for fixing errors
+        self.count = 0  # Number of attempts made to fix the code
 
     def compile_code(self, code, name, SE=0):
+        # Generate filenames based on language
         input_file = name+'.c' if self.language == 'C' else name+'.cpp'
         output_file = name+'.out' if SE == 0 else name+'.bc'
         se_compiler = 'clang' if self.language == 'C' else 'clang++'
+
+        # Write the code to a file
         with open(input_file, 'w') as f:
             f.write(code)
 
-        if SE == 1:
+        # Compile the code
+        if SE == 1:  # If special header is required
             compile_command = [se_compiler,
                                '-I', '/usr/local/include/klee/klee.h',
                                '-emit-llvm', '-c', '-g', '-O0', '-Xclang',
                                '-disable-O0-optnone', input_file,
                                '-o', output_file]
-        else:
+        else:  # If normal compiler is used
             compile_command = [self.compiler,
                                # '-Wall', '-Wextra', '-Werror',
                                input_file,
@@ -34,18 +39,19 @@ class CompilationCheck:
         process = subprocess.run(compile_command, text=True,
                                  capture_output=True)
 
-        if process.returncode != 0:  # Compilation error
+        if process.returncode != 0:  # If there's a compilation error
             return False, process.stderr
         return True, None
 
     def check_and_fix(self, code, error, se=0, name='temp_code'):
+        # Try to compile the code and if it fails, use AI to fix it
         compilable, error = self.compile_code(code, name)
         if not compilable:
             print(Fore.RED + 'Compilation Error: ' + Style.RESET_ALL + error)
             self.count = self.count + 1
             print(Fore.YELLOW + "[" + str(self.count) + "] Attempting to fix compilation errors..."
                   + Style.RESET_ALL)
-            code = self.ai.submit_task(self.prompt + error, code)
+            code = self.ai.submit_task(self.prompt + error, code)  # Ask the AI to fix the error
             return self.check_and_fix(code, error, name)
         self.count = 0
         return code
--- src/performance_check.py
+++ src/performance_check.py
@@ -2,16 +2,20 @@ import subprocess
 from colorama import Fore, Style
 
 
+# This class is used to measure and compare the performance of two pieces of code.
 class PerformanceCheck:
     def __init__(self, compiler, ai, language):
-        self.language = language
-        self.code = ""
+        self.language = language  # Programming language
+        self.code = ""  # The code to be tested
+        # This prompt is used to generate a benchmarking application that measures and compares the performance of the original code and the generated code.
         self.generate_prompt = "Your task is to benchmark the input functions and compare their performance. Write a C++ application to measure the performance of two functions (original and improved codes); add 50 test cases with different complexities to consider various corner cases and give me a better estimation of the time complexity of both programs; run each test case 5 times. I give you the original code and the improved code, and I expect to have one main function that benchmarks both functions and returns one number in the output (no comment, only a single number) that reports the speedup of the improved code (dividing the average time of the improved code by the average time of the original code). Give me only the code and no explanation; save tokens. Say nothing else."
+        # This prompt is used to improve the generated code if it is slower than the original code.
         self.fix_prompt = "The generated code is slower than the original. Please improve the following: "
-        self.compiler = compiler
-        self.ai = ai
+        self.compiler = compiler  # The compiler to use
+        self.ai = ai  # The AI interface to use for generating and fixing the code
 
     def generate_code(self, original_code, generated_code):
+        # Generate a benchmarking application using the AI interface
         prompt = self.generate_prompt + "\n\\original code\n"+original_code\
             + "\n\\improved code\n" + generated_code
         print(Fore.YELLOW + "Generating code..." + Style.RESET_ALL)
@@ -19,19 +23,21 @@ class PerformanceCheck:
         self.code = self.compiler.check_and_fix(self.code, None, name='perf_code')
 
     def measure_performance(self, code):
+        # Measure the performance of the code
         print(Fore.YELLOW + "Measuring performance..." + Style.RESET_ALL)
         process = subprocess.run(['./perf_code.out'], text=True, capture_output=True)
 
-        if process.returncode != 0:  # Runtime error
+        if process.returncode != 0:  # If there's a runtime error
             return None
 
-        return process.stdout  # Speedup
+        return process.stdout  # Return the performance measurement (speedup)
 
     def measure_and_fix(self, input_code, generated_code):
+        # Measure the performance of the code and if it does not meet the performance criteria, use AI to fix it
         self.code = ''
         self.generate_code(input_code, generated_code)
         performance = self.measure_performance(self.code)
-        if performance is None or performance < 1:  # Performance criteria not met
+        if performance is None or performance < 1:  # If the performance criteria are not met
             print(Fore.RED + 'Performance criteria not met: ' + Style.RESET_ALL + performance)
-            generated_code = self.ai.submit_task(self.fix_prompt, generated_code)
+            generated_code = self.ai.submit_task(self.fix_prompt, generated_code)  # Ask the AI to fix the code
             self.verify_and_fix(input_code, generated_code)
